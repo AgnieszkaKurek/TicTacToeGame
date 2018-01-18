25b72ee5c7ee94691989274c358a154cc717f938
AgnieszkaKurek <agniechakurek@gmail.com>
2018-01-18 17:22:15 +0100

    add fild karma.config

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine'],
+      files: [
+        {pattern: 'src/*Spec.js', watched: true},
+      ],
+      preprocessors: {
+        'src/*Spec.js': ['webpack']
+      },
+      webpack: {
+      },
+      webpackMiddleware: {
+        stats: 'errors-only'
+      },
+      client: {
+        clearContext: false
+      },
+      exclude: [],
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false,
+      concurrency: Infinity,
+    })
+  }
