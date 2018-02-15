e116e03924f633604472950f11bc1ad774c692b8
AgnieszkaKurek <agniechakurek@gmail.com>
2018-02-15 08:34:35 +0100

    RenderXorO (#15)
    * add canvas
    
    * add function drawO and drawX , and responsible shape
    
    * formating files

--- src/script.js
+++ src/script.js
@@ -2,12 +2,35 @@
 
 const game = new TicTacToeGame();
 
-function handleMove(position) {
+function drawX(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(25, 25);
+  ctx.lineTo(105, 105);
+  ctx.moveTo(25, 105);
+  ctx.lineTo(105, 25);
+  ctx.strokeStyle = '#388e3c';
+  ctx.lineWidth = 8;
+  ctx.stroke();
+}
+
+function drawO(ctx) {
+  ctx.arc(65, 50, 45, 0, 2 * Math.PI);
+  ctx.lineWidth = 8;
+  ctx.strokeStyle = '#ff3399';
+  ctx.stroke();
+}
+
+
+function handleMove(box) {
+  const canvas = box.childNodes[0];
+  const ctx = canvas.getContext("2d");
+  drawX(ctx);
+  const position = box.getAttribute("data-position");
   console.log(position);
 }
 
 const boxes = document.getElementsByClassName("box");
 for (const box of boxes) {
-  const position = box.getAttribute("data-position");
-  box.addEventListener("click", () => handleMove(position));
+
+  box.addEventListener("click", () => handleMove(box));
 }
