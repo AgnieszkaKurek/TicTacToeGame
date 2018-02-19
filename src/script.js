63072ddde64c01fe5a796da7a3e83f126ae84f46
AgnieszkaKurek <agniechakurek@gmail.com>
2018-02-19 13:41:16 +0100

    remove unnecessary styles , marking private values andadjusting the canvas to the box

--- src/script.js
+++ src/script.js
@@ -4,27 +4,26 @@ const game = new TicTacToeGame();
 
 function drawX(ctx) {
   ctx.beginPath();
-  ctx.moveTo(5, 5);
-  ctx.lineTo(285, 145);
-  ctx.moveTo(5, 145);
-  ctx.lineTo(285, 5);
+  ctx.moveTo(0, 0);
+  ctx.lineTo(150, 150);
+  ctx.moveTo(0, 150);
+  ctx.lineTo(150, 0);
   ctx.strokeStyle = '#388e3c';
   ctx.lineWidth = 10;
   ctx.stroke();
 }
 
 function drawO(ctx) {
-  ctx.arc(150, 80, 60, 0, 2 * Math.PI);
+  ctx.arc(75, 75, 55, 0, 2 * Math.PI, false);
   ctx.lineWidth = 10;
   ctx.strokeStyle = '#ff3399';
   ctx.stroke();
 }
 
-
 function handleMove(box) {
   const canvas = box.childNodes[0];
   const ctx = canvas.getContext("2d");
-  drawX(ctx);
+  drawO(ctx);
   const position = box.getAttribute("data-position");
   console.log(position);
 }
