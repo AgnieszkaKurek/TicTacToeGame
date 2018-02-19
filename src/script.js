382899d5c0ecc972e0c385de45101ad67ab49b94
AgnieszkaKurek <agniechakurek@gmail.com>
2018-02-19 16:31:58 +0100

    RenderXorO (#8)
    * add canvas
    
    * add function drawO and drawX , and responsible shape
    
    * formating files
    
    * removing the error
    
    * removing empty lines
    
    * change the dimensions of characters saved in the canvas
    
    * customizable canvas area for the box area
    
    * remove empty line
    
    * change of dimension canvas
    
    * adding the appropriate dimensions of drawn shapes and format files
    
    * remove unnecessary styles , marking private values andadjusting the canvas to the box
    
    * Remove magic number

--- src/script.js
+++ src/script.js
@@ -1,13 +1,36 @@
 /*global TicTacToeGame*/
 
 const game = new TicTacToeGame();
+const canvasInitialSize = 150;
+const lineWithdth = 10;
 
-function handleMove(position) {
+function drawX(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(canvasInitialSize, canvasInitialSize);
+  ctx.moveTo(0, canvasInitialSize);
+  ctx.lineTo(canvasInitialSize, 0);
+  ctx.strokeStyle = '#388e3c';
+  ctx.lineWidth = lineWithdth;
+  ctx.stroke();
+}
+
+function drawO(ctx) {
+  ctx.arc(canvasInitialSize/2, canvasInitialSize/2, canvasInitialSize/2 - 2 * lineWithdth, 0, 2 * Math.PI, false);
+  ctx.lineWidth = lineWithdth;
+  ctx.strokeStyle = '#ff3399';
+  ctx.stroke();
+}
+
+function handleMove(box) {
+  const canvas = box.childNodes[0];
+  const ctx = canvas.getContext("2d");
+  drawO(ctx);
+  const position = box.getAttribute("data-position");
   console.log(position);
 }
 
 const boxes = document.getElementsByClassName("box");
 for (const box of boxes) {
-  const position = box.getAttribute("data-position");
-  box.addEventListener("click", () => handleMove(position));
+  box.addEventListener("click", () => handleMove(box));
 }
