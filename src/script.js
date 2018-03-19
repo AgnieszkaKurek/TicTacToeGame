867bcbe5db2cd0c0a6572c5dc3db9bcf29df3d48
AgnieszkaKurek <agniechakurek@gmail.com>
2018-03-19 16:41:30 +0100

    Check game state (#19)
    * Fix typo
    
    * checking and displaying the game status
    
    *  add new class and function schow  status
    
    * add new  folder  and function
    
    * add fille
    
    * changes in the layout of the page after displaying the game status
    
    * changes to improve website responsiveness
    
    * removing unnecessary spaces

--- src/script.js
+++ src/script.js
@@ -1,49 +1,8 @@
-/*global TickTackToGameStatus/
-/*global TickTackToGamePlayers*/
 /*global TicTacToeGame*/
+/*global TicTacToeGameRenderer*/
 
-const game = new TicTacToeGame();
-const canvasInitialSize = 150;
-const lineWithdth = 10;
-
-function drawX(box) {
-  const ctx = initDrawing(box);
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  ctx.lineTo(canvasInitialSize, canvasInitialSize);
-  ctx.moveTo(0, canvasInitialSize);
-  ctx.lineTo(canvasInitialSize, 0);
-  ctx.strokeStyle = '#388e3c';
-  ctx.stroke();
-}
-
-function drawO(box) {
-  const ctx = initDrawing(box);
-  ctx.arc(canvasInitialSize / 2, canvasInitialSize / 2, canvasInitialSize / 2 - 2 * lineWithdth, 0, 2 * Math.PI, false);
-  ctx.strokeStyle = '#ff3399';
-  ctx.stroke();
-}
-
-function initDrawing(box) {
-  const canvas = box.childNodes[0];
-  const ctx = canvas.getContext("2d");
-  ctx.lineWidth = lineWithdth;
-  return ctx;
-}
-
-function handleMove(box) {
-  const position = box.getAttribute("data-position");
-  const currentPlayer = game.next;
-  if (game.move(position)) {
-    if (currentPlayer === TickTackToGamePlayers.X) {
-      drawX(box);
-    } else {
-      drawO(box);
-    }
-  }
-}
-
-const boxes = document.getElementsByClassName("box");
-for (const box of boxes) {
-  box.addEventListener("click", () => handleMove(box));
-}
+(function(){
+  const game = new TicTacToeGame();
+  const renderer = new TicTacToeGameRenderer(game);
+  renderer.appendToActions();
+})();
