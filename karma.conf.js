f5ba2c9a599e8a727cd9db5bb18e89bc08b83714
AgnieszkaKurek <agniechakurek@gmail.com>
2018-06-08 13:33:15 +0200

    rename  folder

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
         'src/ticTacToeGame.js',
         'tests/ticTacToeGameSpec.js',
         'tests/ticTacToeGamesScoreSpec.js',
-        'tests/tictacToeGameCombinationSpec.js'
+        'tests/ticTacToeGameCombinationSpec.js'
       ],
       client: {
         clearContext: false
