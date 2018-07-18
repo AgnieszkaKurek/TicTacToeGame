d4a493275ef91245b6d6307aa90c54ad7f5602c5
AgnieszkaKurek <agniechakurek@gmail.com>
2018-07-18 19:13:02 +0200

    CD improvements

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,22 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts/main.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: 'assets', to: 'assets' },
+      { from: 'index.html', to: 'index.html' }
+    ])
+  ],
   mode: 'none',
   entry: './src/index.dev.js',
   devtool: 'inline-source-map',
